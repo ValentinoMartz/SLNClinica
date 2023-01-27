dda2fdaf3399e4467c07ff6af4880f0def082a9b
ValentinoMartz <valentinomartinez55@gmail.com>
2023-01-27 16:04:18 -0300

    Agregar archivos de proyecto.

---
+++ SLNClinica/Models/Medico.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SLNClinica.Models
+{
+    public class Medico
+    {
+        //Ejercicio MODEL-CONTROLLER
+
+        //Tarea 1
+        //Nueva solución: SLNClinica
+
+        //Tarea2
+
+        //Modelo:
+        //Medico
+        //Id
+        //Nombre
+        //Apellido
+        //Matricula
+
+        //Tarea2
+        //Usar validaciones
+        //Nombre y Apellido campos obligatorios
+        //Matrícula → AA1111
+
+        //Tarea 3 
+        //Crear DB: DBClinicaMVC
+
+        //Tarea 4
+        //Programar el controller
+
+        //Index()
+        //Create()
+        //Delete()
+        //Details()
+        //Edit()
+
+    }
+}
